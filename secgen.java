8552bc023798c1db87e4dc492e072ff8ea373aa1
mob2k01 <60192520+mob2k01@users.noreply.github.com>
2020-10-13 18:09:50 +0100

    Update secgen.java

--- secgen.java
+++ secgen.java
@@ -11,24 +11,35 @@ public class Main {
         //gamelog(secret);
         int length = scanner.nextInt();
         secgene(length);
+    }   
+    public static long randomiser() {
+        Random random = new Random();
+        long value = random.nextInt(9);
+        return value;
     }
-   
-    public static void secgene(int length){
+    public static void secgene(int length) {
         if ( length>10 ){
             System.out.println("Error: can't generate a secret number with a length of "+length+" because there aren't enough unique digits.");
         } else {
             long[] ingen = new long[length];
-            //Random random = new Random();
-            for (int i = 0;i < ingen.length-1;i++){
-                //do {
-                    ingen[i] = (long) Math.random(); // ingen [i] = random.nextInt(9) walo
-
-                //} while ( ingen[i]!=0 && i==0);
-
+            for (int i = 0 ; i < ingen.length ; i++ ) {
+                ingen[i] = randomiser();
+                while ( i==0 && ingen[i]==0 ){
+                    ingen[i] = randomiser();
+                }
             }
-            //String secret = new String(ingen.toString());
-            System.out.println("The random secret number is "+ingen+".");
+            for (int i = 0 ; i < ingen.length ; i++ ) {
+                for (int j=0 ; j < ingen.length ; j++ ) {
+                    while ( ingen[i] == ingen[j] && i!=j ) {
+                        ingen[j] = randomiser();
+                    }
+                }
+            }
+            System.out.print("The random secret number is ");
+            for (int i = 0 ; i < ingen.length ; i++ ) {
+                System.out.print(ingen[i]);
+            }
+            System.out.print(" .");
         }
-
     }
 }
